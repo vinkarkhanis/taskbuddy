2f93ccaf3baa5645e15027721a3c5d61a3b4b131
Karkhanis, Vinayak <vinayak.karkhanis@nuance.com>
2024-04-11 18:20:01 +0530

    change

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,6 +10,7 @@ export const Header = () => {
         document.documentElement.removeAttribute("class");
         document.documentElement.classList.add(theme);
     }, [theme]);
+    
 
     return (
         <header>
