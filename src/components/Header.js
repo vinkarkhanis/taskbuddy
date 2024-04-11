5e66707ec254193a45359fc223e76a1ef66c2f95
Karkhanis, Vinayak <vinayak.karkhanis@nuance.com>
2024-04-11 16:02:10 +0530

    Storing theme

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,9 +3,10 @@ import Logo from "../assets/logo.png";
 
 export const Header = () => {
 
-    const [theme, setTheme] = useState("dark");
+    const [theme, setTheme] = useState(JSON.parse(localStorage.getItem("theme")) || "dark");
 
     useEffect(() => {
+        localStorage.setItem("theme", JSON.stringfy(theme));
         document.documentElement.removeAttribute("class");
         document.documentElement.classList.add(theme);
     }, [theme]);
